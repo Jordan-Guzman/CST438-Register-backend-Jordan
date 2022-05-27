4a599bcd2732667d784e88a4411f21df8806cf9f
jordan-guzman <jordanhguz@gmail.com>
2022-05-27 10:03:00 -0700

    system.properties file added and imports modified.

--- cst438_register/src/main/java/com/cst438/controller/StudentController.java
+++ cst438_register/src/main/java/com/cst438/controller/StudentController.java
@@ -28,7 +28,7 @@ import com.cst438.service.GradebookService;
 
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = {"http://localhost:3000", "https://cst438-register-be-guzman.herokuapp.com/"})
 public class StudentController {
 	
 	@Autowired
